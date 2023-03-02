db365d524f8081f32e022c7a5fd8c2ed9af2586a
DustinKeller2 <>
2023-03-02 22:03:07 +0000

    Completed the recursive implementation of the reverse string function

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 //Implement a function that reverses a string using iteration...and then recursion!
-function reverseString(str) {
-  
+
+//Recursive Approach
+function reverse(str) {
+  var ans = "";
+  return function rev(str) {
+    if(str.length < 1) {
+      return ans;
+    }
+    ans += str.charAt(str.length - 1);
+    return rev(str.substring(0, str.length - 1));
+  }
 }
 
-reverseString('yoyo mastery')
+
+const reverseString = reverse();
+console.log(reverseString('yoyo mastery'));
 //should return: 'yretsam oyoy'
